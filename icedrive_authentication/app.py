4aa94b07f11adb0d6deb36a61766920518982ec4
NoeliaOwo <noeliadiazalejoalejo@gmail.com>
2024-01-06 18:51:36 +0100

    Añado Query y Response

--- icedrive_authentication/app.py
+++ icedrive_authentication/app.py
@@ -13,14 +13,16 @@ from .discovery import Discovery
 #Ice.loadSlice('icedrive_authentication/icedrive.ice')
 import IceDrive
 from .authentication import Authentication
-from .delayed_response import AuthenticationQueryResponse
+from .delayed_response import AuthenticationQuery, AuthenticationQueryResponse
 #from icedrive_authentication import authentication
 import IceStorm
+from .discovery import Discovery
+from .persistence import Persistence
 
 
-def anunciar(proxy, servant_proxy):
+def anunciar(discovery_proxy, authentication):
     while True:
-        proxy.announceAuthentication(IceDrive.AuthenticationPrx.uncheckedCast(servant_proxy))
+        discovery_proxy.announceAuthentication(IceDrive.AuthenticationPrx.uncheckedCast(authentication))
         time.sleep(5)  
         
 
@@ -29,6 +31,8 @@ class AuthenticationApp(Ice.Application):
 
     def run(self, args: List[str]) -> int:
         """Execute the code for the AuthentacionApp class."""
+        persistencia = Persistence("client_testing/file.json")
+        
         properties = self.communicator().getProperties()
         topic_name = properties.getProperty("DiscoveryTopic")
         topic_manager = IceStorm.TopicManagerPrx.checkedCast(self.communicator().propertyToProxy("IceStorm.TopicManager.Proxy"))
@@ -42,20 +46,50 @@ class AuthenticationApp(Ice.Application):
         adapter = self.communicator().createObjectAdapter("AuthenticationAdapter")
         adapter.activate()
         
-        servant = Authentication()
-        servant_proxy = adapter.addWithUUID(servant)
-        discovery = Discovery()
-        discovery_proxy = adapter.addWithUUID(discovery)
-        proxy = IceDrive.DiscoveryPrx.uncheckedCast(topic.getPublisher())
         
-        topic.subscribeAndGetPublisher({}, discovery_proxy)
-        threading.Thread(target=anunciar, args=(proxy, servant_proxy), daemon=True).start()
+        # Añado el servant Authentication al adapter
+        authentication_servant = Authentication()
+        authentication = adapter.addWithUUID(authentication_servant)
+        authentication_proxy = IceDrive.AuthenticationPrx.checkedCast(authentication)
+        
+
+        
+        # Añado el servant AuthenticationQuery al adapter
+        authentication_query_servant = AuthenticationQuery()
+        authentication_query = adapter.addWithUUID(authentication_query_servant)
+        authentication_query_topic = topic.subscribeAndGetPublisher({},  authentication_query)
+        authentication_query_proxy = IceDrive.AuthenticationQueryPrx.uncheckedCast(topic.getPublisher())
+    
+        #authentication_query_topic.getRequest()
+        
+        # Añado el servant Discovery al adapter
+        discovery_servant = Discovery()
+        discovery = adapter.addWithUUID(discovery_servant)
+        discovery_topic = topic.subscribeAndGetPublisher({}, discovery)
+        discovery_proxy = IceDrive.DiscoveryPrx.uncheckedCast(topic.getPublisher())
+        
+        
+        # Añado el servant AuthenticationQueryResponse al adapter
+        authentication_query_response_servant = AuthenticationQueryResponse()
+        authentication_query_response = adapter.addWithUUID(authentication_query_response_servant)
+        authentication_query_response_proxy = IceDrive.AuthenticationQueryResponsePrx.uncheckedCast(topic.getPublisher())
+        
+        
+        
+        #threading.Thread(target=anunciar, args=(discovery_proxy, authentication), daemon=True).start()
+        
+        #if persistencia.check_user("Noelia", "1234"):
+        #    authentication_proxy.login("Noelia","1234", None)
+        #else:
+        #    authentication_query_proxy.login("","", authentication_query_response_proxy)
+        
+        
+
     
         
-        with open('server_proxy.txt', 'w') as f:
-            f.write(str(servant_proxy))
+
         
-        logging.info("Proxy: %s", servant_proxy)
+        logging.info("Proxy: %s", authentication)
         
         self.shutdownOnInterrupt()
         self.communicator().waitForShutdown()
